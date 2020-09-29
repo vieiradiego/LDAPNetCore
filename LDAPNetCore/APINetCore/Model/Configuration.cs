569f65dbf5620f92bec0a0dab10df4f344315277
Diego Moreira Vieira <diegovieira.ti@gmail.com>
2020-09-29 07:57:31 -0300

    Include integration on Teams

---
+++ LDAPNetCore/APINetCore/Model/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APINetCore.Model
+{
+    public class Configuration
+    {
+    }
+}
